0b02ba61ab9e5fef06764e24429599030ba5eaa1
bigGreenPeople <1243596620@qq.com>
2017-09-17 10:12:13 +0800

    maven整合web工程

---
+++ maven01/src/main/java/maven01/MavenServlet.java
@@ -0,0 +1,26 @@
+package maven01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MavenServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		System.out.println("你好maven");
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doGet(request, response);
+	}
+
+}
